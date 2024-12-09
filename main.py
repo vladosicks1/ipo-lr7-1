b3a56d856e7fb7696bbfe5b4e11a0b4389c24e08
vladosicks1 <goborovvlad@gmail.com>
2024-12-09 14:25:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+
+books = {
+    1: {"title": "Война и мир", "author": "Лев Толстой", "year": 1869},
+    2: {"title": "Дом, в котором", "author": "Мариам Петросян", "year": 2010},
+    3: {"title": "1984", "author": "Джордж Оруэлл", "year": 1949},
+    4: {"title": "Мастер и Маргарита", "author": "Михаил Булгаков", "year": 1967},
+    5: {"title": "Гарри Поттер и философский камень", "author": "Джоан Роулинг", "year": 1997}
+}
+
+count = 1
+
+for key,book in books.items():
+    print("-"*25,"Книга",count,"-"*25)
+    print("Название:",book['title'],end=", ")
+    print("Автор:",book['author'])
+    print("-"*25,book['year'],"-"*25)
+    count+=1
